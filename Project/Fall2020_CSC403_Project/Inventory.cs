8eda9138abda45f85543557058cc5a9b5bed5a66
Hannah Folkertsma <hannahfolkertsma@gmail.com>
2021-10-25 16:48:28 -0500

    added basic inventory and item classes

---
+++ Project/Fall2020_CSC403_Project/Inventory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2020_CSC403_Project
+{
+    class Inventory
+    {
+        int walletBalance;
+        List<Item> Contents = new List<Item>();
+    }
+}
---
+++ Project/Fall2020_CSC403_Project/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2020_CSC403_Project
+{
+    class Item
+    {
+        string ItemName;
+        string ItemDescription;
+        int ItemPrice;
+        public Item(string name, string description, int price)
+        {
+            ItemName = name;
+            ItemDescription = description;
+            ItemPrice = price;
+        }
+    }
+}
