a354a4e76368a2310476f0b1eb451e0592ab7561
Hannah Folkertsma <hannahfolkertsma@gmail.com>
2021-10-27 17:20:24 -0500

    changed getters

--- Project/MyGameLibrary/Shop/Item.cs
+++ Project/MyGameLibrary/Shop/Item.cs
@@ -4,36 +4,16 @@ namespace Fall2020_CSC403_Project
 {
     public class Item
     {
-        string ItemName;
-        string ItemDescription;
-        int ItemPrice;
-        Items ItemID;
+        string ItemName { get; set; }
+        string ItemDescription { get; set; }
+        int ItemPrice { get; set; }
+        Items ItemID { get; set; }
         public Item(Items identifier, string name, string description, int price)
         {
             ItemID = identifier;
             ItemName = name;
             ItemDescription = description;
             ItemPrice = price;
-        }
-
-        public int getPrice()
-        {
-            return ItemPrice;
-        }
-
-        public string getDescription()
-        {
-            return ItemDescription;
-        }
-
-        public string getName()
-        {
-            return ItemName;
-        }
-
-        public Items getID()
-        {
-            return ItemID;
         }
     }
 }
