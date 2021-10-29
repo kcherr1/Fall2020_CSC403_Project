6ee7215165150b3b43a8e88c087de9ef87c7c0f8
Hannah Folkertsma <hannahfolkertsma@gmail.com>
2021-10-29 11:22:14 -0500

    fixed location ratio

--- Project/Fall2020_CSC403_Project/TextEngine.cs
+++ Project/Fall2020_CSC403_Project/TextEngine.cs
@@ -9,15 +9,15 @@ namespace Fall2020_CSC403_Project
     public partial class TextEngine : Form
     {
         private Story story { get; set; }
-        private double ForegroundImage_Xscale{get;set; }
+        private double ForegroundImage_Xscale { get; set; }
         private double ForegroundImage_Yscale { get; set; }
         public TextEngine()
         {
             this.story = new Story("\\Fall2021_CSC403_Project\\Project\\Fall2020_CSC403_Project\\data\\", "Story.txt");
             string line = story.GetNextLine();
             InitializeComponent();
-            ForegroundImage_Xscale = (double)ForegroundImage.Location.X / Width;
             Console.WriteLine(ForegroundImage.Location);
+            ForegroundImage_Xscale = (double)ForegroundImage.Location.X / Width;
             ForegroundImage_Yscale = (double)ForegroundImage.Location.Y / Height;
             this.ForegroundImage.BackColor = Color.Transparent;
             this.ChangeText(line);
@@ -61,12 +61,12 @@ namespace Fall2020_CSC403_Project
 
         private void ForegroundImage_Resize(object sender, EventArgs e)
         {
-            Control control = (Control)sender;
-            Console.WriteLine("foregroundimage resize event");
             double ratio = this.ForegroundImage.Size.Width / (this.ForegroundImage.Size.Height * 1.0);
             ForegroundImage.Size = new Size((int)(ClientRectangle.Height * ratio), (int)ClientRectangle.Height);
-            Console.WriteLine(ForegroundImage.Location);
-            ForegroundImage.Location = new Point((int)ForegroundImage_Xscale*Width, (int)ForegroundImage_Yscale*Height);
+            if (ForegroundImage_Xscale != 0 && ForegroundImage_Yscale !=0)
+            {
+                ForegroundImage.Location = new Point((int)Math.Floor(ForegroundImage_Xscale * Width), (int)Math.Floor(ForegroundImage_Yscale * Height));
+            }
         }
     }
 }
