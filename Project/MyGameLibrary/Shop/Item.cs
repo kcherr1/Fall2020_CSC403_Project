5a1498917521380a81265623173e8daa68ad28ae
Hannah Folkertsma <hannahfolkertsma@gmail.com>
2021-10-29 08:43:38 -0500

    added "used" property to the item class

--- Project/MyGameLibrary/Shop/Item.cs
+++ Project/MyGameLibrary/Shop/Item.cs
@@ -1,4 +1,5 @@
 ﻿using MyGameLibrary.Shop;
+using System;
 
 namespace Fall2020_CSC403_Project
 {
@@ -8,12 +9,14 @@ namespace Fall2020_CSC403_Project
         public string ItemDescription { get; set; }
         public int ItemPrice { get; set; }
         public Items ItemID { get; set; }
+        public Boolean used {get; set;}
         public Item(Items identifier, string name, string description, int price)
         {
             ItemID = identifier;
             ItemName = name;
             ItemDescription = description;
             ItemPrice = price;
+            used = false;
         }
     }
 }
