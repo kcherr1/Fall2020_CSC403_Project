cf5acbe49903407c57631da47a8d674cbc8b230f
TempusEstNunc <49039565+TempusEstNunc@users.noreply.github.com>
2021-10-29 16:00:19 -0500

    Created character class and function for updating love scores

---
+++ Project/MyGameLibrary/Character/Character.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Fall2020_CSC403_Project.code
+{
+    public class Character
+    {
+        public string CharacterName;
+        public int LoveScore;
+        public Dictionary<ItemID, LoveValue> ItemScores;
+
+        public Character(string name, int LScore, Dictionary<ItemID, LoveValue> dict)
+        {
+            CharacterName = name;
+            LoveScore = LScore;
+            ItemScores = dict;
+        }
+
+        public void LoveUpdate(Character Person, int AdditionalScore)
+        {
+            Person.LoveScore = AdditionalScore + Person.LoveScore;
+        }
+    }
+}
