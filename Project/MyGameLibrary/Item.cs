223fa9ef0a1b806ccfa3a9acbf29462dfe35e953
vynleran <pjahanlou@gmail.com>
2021-11-01 15:35:52 -0500

    Created the inventory UI

--- Project/MyGameLibrary/Item.cs
+++ Project/MyGameLibrary/Item.cs
@@ -7,8 +7,19 @@ namespace Fall2020_CSC403_Project.code
 	public class Item
 	{
 
+		private string type = null;
+
 		public Item()
 		{
+
+		}
+
+		public string getType() {
+			return this.type;
+		}
+
+		public void setType(string type) {
+			this.type = type;
 		}
 	}
 }
