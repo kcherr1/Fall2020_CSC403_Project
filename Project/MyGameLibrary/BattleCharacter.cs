6d1f9df4fb542e44a82b22dbcb9d90d9c962ea26
Scags <johnmascagni@gmail.com>
2021-11-01 15:37:10 -0500

    Create skills

--- Project/MyGameLibrary/BattleCharacter.cs
+++ Project/MyGameLibrary/BattleCharacter.cs
@@ -5,35 +5,161 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 #pragma warning disable 1591 // use this to disable comment warnings
 
-namespace Fall2020_CSC403_Project.code {
-  public class BattleCharacter : Character {
-    public int Health { get; private set; }
-    public int MaxHealth { get; private set; }
-    private float strength;
+namespace Fall2020_CSC403_Project.code
+{
+	public class BattleCharacter : Character
+	{
+		// Walk over the health skill to keep everyone sane
+		// This way we can use the same code that's provide but wrap over the health skill
+		public int Health
+		{
+			get
+			{
+				return Skills[Defs.SKILL_HP].CurrentLevel;
+			}
+			private set
+			{
+				Skills[Defs.SKILL_HP].SetCurrentLevel(value);
+			}
+		}
+		public int MaxHealth
+		{
+			get
+			{
+				return Skills[Defs.SKILL_HP].Level;
+			}
+			private set
+			{
+				Skills[Defs.SKILL_HP].SetLevel(value);
+			}
+		}
 
-    // ???? wtf is this?
-    // This is the variable for dynamically be applied to the health of the enemy or player
-    // dumb as shit
-    public event Action<int> AttackEvent;
+		public Dictionary<string, Skill> Skills { get; private set; }
 
-    // The constructor for the BattleCharacter class
-    public BattleCharacter(Vector2 initPos, Collider collider) : base(initPos, collider) {
-      MaxHealth = 20;
-      strength = 2;
-      Health = MaxHealth;
-    }
+		// ???? wtf is this?
+		// This is the variable for dynamically be applied to the health of the enemy or player
+		// dumb as shit
+		public event Action<string, int> AttackEvent;
 
-    // Method for how much damage you apply to alter the health of the BattleCharacter
-    public void OnAttack(int amount) {
-      AttackEvent((int)(amount * strength));
-    }
+		public Image CombatImage { get; protected set; }
 
-    // Method for decreasing the health of the BattleCharacter
-    public void AlterHealth(int amount) {
-      Health += amount;
-    }
-  }
+		// Combat level is the highest skill level + a calculated max health bonus
+		public int CombatLevel
+		{
+			get
+			{
+				return HighestSkillLevel() + MaxHealth / 5;
+			}
+		}
+
+		public BattleCharacter(Vector2 initPos, Collider collider) : base(initPos, collider)
+		{
+			Skills = new Dictionary<string, Skill>();
+			AddSkill(new Skill(Defs.SKILL_MELEE));
+			AddSkill(new Skill(Defs.SKILL_RANGED));
+			AddSkill(new Skill(Defs.SKILL_MAGIC));
+			AddSkill(new Skill(Defs.SKILL_HP));
+
+			// Default health to 20, will be changed later
+			SetMaxHealth(20);
+		}
+
+		public void OnAttack(string skillType)
+		{
+			int maxhit = AttackStrength(skillType);
+
+			// Add in randomness, in some cases the attack can miss
+			// maxhit +1 because of inclusion
+			int damage = Helpers.Random(0, maxhit + 1);
+			AttackEvent(skillType, damage);
+		}
+
+		// Wrap over a much more well-worded function
+		public void AlterHealth(int amount)
+		{
+			if (amount < 0)
+			{
+				Damage(-amount);
+			}
+			else
+			{
+				Heal(amount);
+			}
+		}
+		// Subtract health from the character
+		public void Damage(int amount)
+		{
+//			Console.WriteLine("Health before: {0}", Health);
+			Health -= amount;
+//			Console.WriteLine("Health after: {0}", Health);
+			if (Health < 0)
+			{
+				Health = 0;
+				// Die();
+			}
+		}
+		// Add health to the character
+		public void Heal(int amount)
+		{
+			Health += amount;
+			if (Health > MaxHealth)
+			{
+				Health = MaxHealth;
+			}
+		}
+		public void FullyHeal()
+		{
+			Health = MaxHealth;
+		}
+		protected void SetMaxHealth(int amount)
+		{
+			MaxHealth = amount;
+		}
+
+		private void AddSkill(Skill skill)
+		{
+			Skills.Add(skill.Name, skill);
+		}
+
+		public int HighestSkillLevel()
+		{
+			int highest = 0;
+			foreach (var item in Skills)
+			{
+				// Skip health because it's special
+				if (item.Key == Defs.SKILL_HP)
+				{
+					continue;
+				}
+				if (highest < item.Value.Level)
+					highest = item.Value.Level;
+			}
+			return highest;
+		}
+
+		public int AttackStrength(string skillType)
+		{
+			int currLevel = Skills[skillType].Level / 4 + 1;
+			int bonus = EquipmentBonus(Skills[skillType]);
+
+			// Level 1 skills need a little nudge
+			int additive = 1;
+			return currLevel + bonus + additive;
+		}
+
+		private void SetSkill(string skill, int level)
+		{
+			Skills[skill].SetLevel(level);
+		}
+
+		// To be overridden by the Player class
+		public virtual int EquipmentBonus(Skill skill)
+		{
+			return 0;
+		}
+	}
 }
--- Project/MyGameLibrary/Player.cs
+++ Project/MyGameLibrary/Player.cs
@@ -7,7 +7,26 @@ using System.Threading.Tasks;
 namespace Fall2020_CSC403_Project.code {
   public class Player : BattleCharacter {
     public Player(Vector2 initPos, Collider collider) : base(initPos, collider) {
+      AttackEvent += GainExperience;
 
+      // Attach the player combat image
+      CombatImage = new System.Drawing.Bitmap(Defs.IMG_PLAYER);
+    }
+
+    public override int EquipmentBonus(Skill skill)
+    {
+      return 0;
+    }
+
+    private void GainExperience(string skillType, int damage)
+    {
+      // Gain experience depending on the type of skill used
+      int exp = damage * 4;
+      Skills[skillType].AddExperience(exp);
+
+      // Globally increment health exp
+      int hpexp = damage;
+      Skills[Defs.SKILL_HP].AddExperience(hpexp);
     }
   }
 }
