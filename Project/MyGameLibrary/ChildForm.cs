7370829d03244e1b6760de4a41ee49e0694da956
Tachnyan <90418666+Tachnyan@users.noreply.github.com>
2021-11-03 14:40:54 -0500

    Setup wrapper classes for Forms class to handle creating childforms for a hostform. Converted current forms into one form.

---
+++ Project/MyGameLibrary/ChildForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fall2020_CSC403_Project.code
+{
+    public class ChildForm : Form
+    {
+
+        public event EventHandler<ShowRequestArgs> ShowRequest;
+        public event EventHandler<HideRequestArgs> HideRequest;
+        public event EventHandler<ChildCreateChildArgs> ChildCreateChild;
+        public ChildForm Creator;
+        public HostForm Parent;
+        public ChildForm()
+        {
+            FormBorderStyle = FormBorderStyle.None;
+        }
+
+        protected ChildForm CreateChild(ChildForm newChild)
+        {
+            ChildCreateChildArgs args = new ChildCreateChildArgs();
+            args.creator = this;
+            args.created = newChild;
+
+            EventHandler<ChildCreateChildArgs> handler = ChildCreateChild;
+            handler(this, args);
+            return newChild;
+        }
+
+        public void RequestHide()
+        {
+            HideRequestArgs args = new HideRequestArgs();
+            args.child = this;
+            EventHandler<HideRequestArgs> handler = HideRequest;
+            handler(this, args);
+        }
+
+        public void RequestShow()
+        {
+            ShowRequestArgs args = new ShowRequestArgs();
+            args.child = this;
+            EventHandler<ShowRequestArgs> handler = ShowRequest;
+            handler(this, args);
+
+        }
+
+        protected void CloseCleanly()
+        {
+            Creator.RequestShow();
+            Close();
+        }
+    }
+}
---
+++ Project/MyGameLibrary/HostForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fall2020_CSC403_Project.code
+{
+    public class HostForm : Form
+    { 
+
+
+        public HostForm()
+        {
+            IsMdiContainer = true;
+        }
+
+        protected ChildForm CreateChild(ChildForm newChild)
+        {
+            newChild.MdiParent = this;
+            newChild.Parent = this;
+            newChild.ShowRequest += OnShowRequest;
+            newChild.HideRequest += OnHideRequest;
+            newChild.ChildCreateChild += OnChildCreateChild;
+
+            return newChild;
+        }
+
+        protected void OnShowRequest(object sender, ShowRequestArgs e)
+        {
+            e.child.Show();
+        }
+
+        protected void OnHideRequest(object sender, HideRequestArgs e)
+        {
+            e.child.Hide();
+        }
+
+        protected void OnChildCreateChild(object sender, ChildCreateChildArgs e)
+        {
+            e.created.MdiParent = this;
+            e.created.Creator = e.creator;
+
+        }
+    }
+
+
+    public class ShowRequestArgs : EventArgs
+    {
+        public ChildForm child { get; set; }
+    }
+
+    public class HideRequestArgs : EventArgs
+    {
+        public ChildForm child { get; set; }
+    }
+
+    public class ChildCreateChildArgs : EventArgs
+    {
+        public ChildForm creator { get; set; }
+        public ChildForm created { get; set; }
+    }
+
+}
