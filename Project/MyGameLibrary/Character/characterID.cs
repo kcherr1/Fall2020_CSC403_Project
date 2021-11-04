0fae63881c86340c6c2b0899b215c6dfa1faea49
TempusEstNunc <49039565+TempusEstNunc@users.noreply.github.com>
2021-11-04 14:27:00 -0500

    part of char ID done

---
+++ Project/MyGameLibrary/Character/characterID.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGameLibrary.Character
+{
+    public enum CharID
+    {
+        Wendy = 1,
+        Tony = 2,
+        Green = 3,
+        Ronald = 4,
+        BurgerKing = 5
+    }
+}
