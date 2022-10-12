d16a97420001f3c00fa9f05bac7f7ec781c4137d
Reid Trisler <rtrisler@fenwaygroup.com>
2022-10-12 12:10:48 -0700

    Added two forms: One for start/pause menu and one for help menu

---
+++ Project/Fall2020_CSC403_Project/FrmHelpScreen.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Fall2020_CSC403_Project
+{
+    partial class FrmHelpScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmHelpScreen));
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("MS Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(113, 78);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(861, 425);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cool Game Alert Help Screen\r\nWelcome to Cool Game Alert!\r\n\r\nTo play the game, ref" +
+    "er to the controls below:\r\n\r\nW A S D to move the player\r\n\r\n...More about the gam" +
+    "e here\r\n\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // FrmHelpScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FrmHelpScreen";
+            this.Text = "FrmHelpScreen";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Project/Fall2020_CSC403_Project/FrmMenuScreen.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Fall2020_CSC403_Project
+{
+    partial class FrmMenuScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMenuScreen));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Start = new System.Windows.Forms.Button();
+            this.Help = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label1.Location = new System.Drawing.Point(248, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(308, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cool Game Alert";
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.Color.SeaShell;
+            this.Start.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Start.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Start.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start.ForeColor = System.Drawing.Color.Black;
+            this.Start.Location = new System.Drawing.Point(312, 199);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(173, 28);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            // 
+            // Help
+            // 
+            this.Help.BackColor = System.Drawing.Color.SeaShell;
+            this.Help.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Help.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Help.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Help.ForeColor = System.Drawing.Color.Black;
+            this.Help.Location = new System.Drawing.Point(312, 250);
+            this.Help.Name = "Help";
+            this.Help.Size = new System.Drawing.Size(173, 28);
+            this.Help.TabIndex = 2;
+            this.Help.Text = "Help";
+            this.Help.UseVisualStyleBackColor = false;
+            // 
+            // MenuScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 551);
+            this.Controls.Add(this.Help);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.label1);
+            this.Name = "MenuScreen";
+            this.Text = "MenuScreen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Help;
+    }
+}
