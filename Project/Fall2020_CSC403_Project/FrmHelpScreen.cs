1fcd45066d0e895bfd57d6a81ab2cf2261ffaada
Reid Trisler <109759915+R-Trisler@users.noreply.github.com>
2022-10-19 13:22:33 -0700

    Started Implementing Snake MiniGame
    Created new forms and classes to support the snake game

---
+++ Project/Fall2020_CSC403_Project/FrmHelpScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fall2020_CSC403_Project
+{
+    public partial class FrmHelpScreen : Form
+    {
+        public FrmHelpScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
