b433a1ef028ad048525a32970c861e832eb80b70
antezovko23 <antezovko.az@gmail.com>
2022-10-25 03:21:59 -0500

    Database Integration

---
+++ Project/MyGameLibrary/Models/StatisticsModel.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGameLibrary.Models
+{
+    public class StatisticsModel
+    {
+
+        public static uint DamageDone { get; set; }
+        
+        public static uint DamageTaken { get; set; }
+        
+        public static float DistanceTraveled { get; set; }
+        
+    }
+
+    public class StatisticsDatabaseModel
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public uint DamageDone { get; set; }
+        public uint DamageTaken { get; set; }
+        public float DistanceTraveled { get; set; }
+    }
+}
