c3b6fbfe63c19224b091b9906c74cb2d242baaa5
brendon-burd <62077193+brendon-burd@users.noreply.github.com>
2022-10-26 16:39:54 -0500

    Added system to track inventory

---
+++ Project/MyGameLibrary/Inventoy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#pragma warning disable 1591 // use this to disable comment warnings
+
+namespace Fall2020_CSC403_Project.code {
+    public class Inventory {
+        List<object> inventoryList= new List<object>{};
+
+        public void add(object newItem)
+        {
+            inventoryList.Add(newItem);
+        }
+
+        public List<object> getInventoryList()
+        {
+            return inventoryList;
+        }
+    }
+}
