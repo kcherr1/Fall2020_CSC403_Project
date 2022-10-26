6d279ac728cb08d41f43d8b2b3f243ad70d815b7
Reid Trisler <80478785+RTrisler@users.noreply.github.com>
2022-10-26 15:16:57 -0500

    ImplementFloor1
    Started creating assets needed to design level.

--- Project/Fall2020_CSC403_Project/FrmLevel.cs
+++ Project/Fall2020_CSC403_Project/FrmLevel.cs
@@ -20,8 +20,8 @@ namespace Fall2020_CSC403_Project {
     }
 
     private void FrmLevel_Load(object sender, EventArgs e) {
-      const int PADDING = 7;
-      const int NUM_WALLS = 13;
+      const int PADDING = 1;
+      const int NUM_WALLS = 4;
 
       player = new Player(CreatePosition(picPlayer), CreateCollider(picPlayer, PADDING));
       bossKoolaid = new Enemy(CreatePosition(picBossKoolAid), CreateCollider(picBossKoolAid, PADDING));
@@ -142,5 +142,10 @@ namespace Fall2020_CSC403_Project {
     private void lblInGameTime_Click(object sender, EventArgs e) {
 
     }
-  }
+
+        private void picWall1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
