0d88fe013d7139cb0217971e40f3094788272d9e
1234thien <1234thien@gmail.com>
2022-10-27 19:05:04 -0500

    fixed bug (hopefully)

---
+++ Project/SaveSystem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Fall2020_CSC403_Project
+{
+
+	public struct SaveStruct
+	{
+		public int health;
+		public int maxHealth;
+		public int experience;
+        public float playerStrength;
+        public float playerDefense;
+        public float playerSpeed;
+		public float level;
+		public Vector2 playerPos;
+		public BattleCharacter[] enemies;
+
+
+		public SaveStruct(Player player, Vector2 playerPos, BattleCharacter[] enemies)
+		{
+			this.health = player.Health;
+			this.maxHealth = player.MaxHealth;
+			this.playerStrength = player.playerStrength;
+			this.playerDefense = player.playerDefense;
+			this.playerSpeed = player.playerSpeed;
+			this.level = player.level;
+			this.playerPos = playerPos;
+			this.enemies =	enemies;
+		}
+
+	}
+
+	public class SaveSystem
+	{
+
+		public SaveSystem()
+		{
+
+		}
+
+
+		
+	}
+}
