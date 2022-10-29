f382e3c9521d4040486f515a48602e875821c885
Kennedy Ford <knf010@email.latech.edu>
2022-10-29 10:50:45 -0500

    added deasth sequence form class

---
+++ Project/Fall2020_CSC403_Project/FrmDeath.cs
@@ -0,0 +1,25 @@
+﻿using Fall2020_CSC403_Project.code;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Fall2020_CSC403_Project
+{
+    public partial class FrmDeath : Form
+    {
+        public static FrmDeath instance = null;
+
+        private FrmDeath()
+        {
+            InitializeComponent();
+        }
+
+        public static FrmDeath GetInstance()
+        {
+            instance = new FrmDeath();
+            return instance;
+        }
+    }
+}
+
