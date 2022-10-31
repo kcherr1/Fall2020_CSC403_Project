cf41be7fd365685b7f9acecf9d303d920cf72b84
Joshua Authement <71782760+jga029@users.noreply.github.com>
2022-10-30 20:48:05 -0500

    Update HealthItem.cs

--- Project/Fall2020_CSC403_Project/HealthItem.cs
+++ Project/Fall2020_CSC403_Project/HealthItem.cs
@@ -8,11 +8,14 @@ namespace Fall2020_CSC403_Project
 	public class HealthItem : Item
 	{
 		public int Health { get; set; }
+	// HealthItem constructor w/ paremeters
         public HealthItem(int itemNum, string name, int health) 
             : base(itemNum, name)
         {
+	    // HealthItem properties
             Health = health;
         }
+	//create new instance of HealthItem
         public new HealthItem Clone()
         {
             return new HealthItem(ItemNum, Name, Health);
