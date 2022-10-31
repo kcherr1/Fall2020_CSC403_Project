5309da418003131c8d525bddf08b9879bf700540
Joshua Authement <71782760+jga029@users.noreply.github.com>
2022-10-30 20:25:45 -0500

    Update ItemInfo.cs

--- Project/Fall2020_CSC403_Project/ItemInfo.cs
+++ Project/Fall2020_CSC403_Project/ItemInfo.cs
@@ -10,16 +10,22 @@ namespace Fall2020_CSC403_Project.code
 		private static List<Item> _gameItem;
 		static ItemInfo()
 		{
+			// creating the items for the game
 			_gameItem = new List<Item>();
+			// creating first weapon
 			_gameItem.Add(new Weapon(01, "Stapler", 1);
+			// creating first health item
 			_gameItem.Add(new HealthItem(02, "Health", 10);
 		}
 	}
+	// instantiates the items
 	public static Item CreateItem(int itemNum)
 	{
+		// Looks through list for item with the itemNum
 		Item regularItem = _gameItem.FirstOrDefault(item => item.ItemNum == itemNum);
 		if(regularItem != null)
 		{
+			// if there is a matach, a new instance of the item is created
 			return regularItem.Clone();
 		}
 		return null;
