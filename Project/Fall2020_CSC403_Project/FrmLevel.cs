176c97d1ec1ae8d28842d8f72f08b14e953f9445
Sara-Grey <sara.grey1080@gmail.com>
2022-10-31 14:47:11 -0500

    Muisc fixed

--- Project/Fall2020_CSC403_Project/FrmLevel.cs
+++ Project/Fall2020_CSC403_Project/FrmLevel.cs
@@ -46,6 +46,7 @@ namespace Fall2020_CSC403_Project {
       const int PADDING = 7;
       const int NUM_WALLS = 13;
       playerIsDead = false;
+      level_music.PlayLooping();
 
       player = new Player(CreatePosition(picPlayer), CreateCollider(picPlayer, PADDING));
       bossKoolaid = new Enemy(CreatePosition(picBossKoolAid), CreateCollider(picBossKoolAid, PADDING));
