2abdf7afca160854694279c4dfe50e7bcd5c33f8
Sara-Grey <sara.grey1080@gmail.com>
2022-11-16 10:47:25 -0600

    Victory

--- Project/Fall2020_CSC403_Project/Properties/Resources.Designer.cs
+++ Project/Fall2020_CSC403_Project/Properties/Resources.Designer.cs
@@ -400,6 +400,16 @@ namespace Fall2020_CSC403_Project.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap skulls {
+            get {
+                object obj = ResourceManager.GetObject("skulls", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Sound_btn {
             get {
                 object obj = ResourceManager.GetObject("Sound_btn", resourceCulture);
