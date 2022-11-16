38b353cf9634dca14e34240f209bc98538d05373
Sara-Grey <sara.grey1080@gmail.com>
2022-11-16 12:49:36 -0600

    Victory Scene

--- Project/Fall2020_CSC403_Project/Properties/Resources.Designer.cs
+++ Project/Fall2020_CSC403_Project/Properties/Resources.Designer.cs
@@ -81,6 +81,16 @@ namespace Fall2020_CSC403_Project.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap background {
+            get {
+                object obj = ResourceManager.GetObject("background", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
         /// </summary>
         internal static System.IO.UnmanagedMemoryStream battle_music {
