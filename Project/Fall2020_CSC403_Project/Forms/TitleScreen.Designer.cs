7975d2113d7f74af55f12ff76b173e832f6a9a48
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2023-10-15 00:01:52 -0500

    added button

--- Project/Fall2020_CSC403_Project/Forms/TitleScreen.Designer.cs
+++ Project/Fall2020_CSC403_Project/Forms/TitleScreen.Designer.cs
@@ -29,20 +29,34 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(167, 160);
+            this.label1.Location = new System.Drawing.Point(216, 157);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(419, 90);
             this.label1.TabIndex = 0;
             this.label1.Text = "Game Title";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(231, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(380, 65);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // TitleScreen
             // 
@@ -50,7 +64,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.ClientSize = new System.Drawing.Size(860, 484);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(860, 484);
             this.Name = "TitleScreen";
             this.Text = "Start Game";
             this.ResumeLayout(false);
@@ -61,5 +77,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Project/Fall2020_CSC403_Project/Forms/TitleScreen.cs
+++ Project/Fall2020_CSC403_Project/Forms/TitleScreen.cs
@@ -13,30 +13,91 @@ namespace Fall2020_CSC403_Project
 {
     public partial class TitleScreen : Form
     {
+        private FontFamily titleFont;
+        private Font titleFontStyle; // Store the title font style
+
         public TitleScreen()
         {
             InitializeComponent();
-            setTitleFont();
-            setTitleLocation();
+            InitializeFonts();
+            InitializeUI();
+            this.SizeChanged += TitleScreen_SizeChanged;
         }
 
-        //leave empty, needed to render label
-        private void label1_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void setTitleFont()
+        private void InitializeFonts()
         {
             PrivateFontCollection pfc = new PrivateFontCollection();
             pfc.AddFontFile("./Fonts/pixelated.ttf");
-            label1.Font = new Font(pfc.Families[0], 48, FontStyle.Regular);
+            titleFont = pfc.Families[0];
         }
 
-        private void setTitleLocation()
+        private void InitializeUI()
         {
-            label1.Location = new Point((this.ClientSize.Width - label1.Width) / 2, (this.ClientSize.Height - label1.Height) / 4);
+            SetTitleFont();
+            SetTitleLocation();
+            SetButtonStyle();
+            SetButtonSize();
+            SetButtonLocation();
+            
         }
 
+        private void SetTitleFont()
+        {
+            float fontSize = this.ClientSize.Width / 12;
+            FontStyle fontStyle = FontStyle.Regular;
+
+            titleFontStyle = new Font(titleFont, fontSize, fontStyle);
+            label1.Font = titleFontStyle;
+        }
+
+        private void SetTitleLocation()
+        {
+            int x = (this.ClientSize.Width - label1.Width) / 2;
+            int y = (this.ClientSize.Height - label1.Height) / 5;
+            label1.Location = new Point(x, y);
+        }
+
+        private void SetButtonStyle()
+        {
+            float buttonFontSize = this.ClientSize.Width / 24; // You can adjust this value as needed
+            FontStyle buttonFontStyle = FontStyle.Regular; // Set your desired style here
+
+            Font buttonFont = new Font(titleFont, buttonFontSize, buttonFontStyle);
+            button1.Font = buttonFont;
+        }
+
+        private void SetButtonLocation()
+        {
+            button1.Location = new Point((this.ClientSize.Width - button1.Width) / 2, (this.ClientSize.Height - button1.Height) / 2);
+        }
+
+        private void TitleScreen_SizeChanged(object sender, EventArgs e)
+        {
+            SetTitleFont();
+            SetTitleLocation();
+            SetButtonStyle();
+            SetButtonSize();
+            SetButtonLocation();
+        }
+
+        private void SetButtonSize()
+        {
+            int buttonWidth = this.ClientSize.Width / 4;
+            int buttonHeight = this.ClientSize.Height / 10;
+
+            button1.Size = new Size(buttonWidth, buttonHeight);
+        }
+
+        // Leave empty, needed to render the button
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Leave empty, needed to render the label
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
