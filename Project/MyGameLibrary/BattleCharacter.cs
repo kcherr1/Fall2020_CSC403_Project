c54be44db54c15251cd2e8aef10c99a7e925ca2e
Jonah Fitzgerald <jonahfitzgerald0113@gmail.com>
2023-10-19 14:59:17 -0500

    Changed the strength multiplier in AttackEvent to add strength (makes the scaling better); added strength and health increases when leveling up

--- Project/MyGameLibrary/BattleCharacter.cs
+++ Project/MyGameLibrary/BattleCharacter.cs
@@ -8,9 +8,9 @@ using System.Threading.Tasks;
 
 namespace Fall2020_CSC403_Project.code {
   public class BattleCharacter : Character {
-    public int Health { get; private set; }
-    public int MaxHealth { get; private set; }
-    private float strength;
+    public int Health { get; protected set; }
+    public int MaxHealth { get; protected set; }
+    public float strength { get; protected set; }
 
     public int experience { get; protected set; }
 
@@ -18,13 +18,14 @@ namespace Fall2020_CSC403_Project.code {
 
     public BattleCharacter(Vector2 initPos, Collider collider) : base(initPos, collider) {
       MaxHealth = 20;
-      strength = 2;
+      strength = 0;
       experience = 10;
       Health = MaxHealth;
     }
 
+    //amount should be a negative number, so subtract strength
     public void OnAttack(int amount) {
-      AttackEvent((int)(amount * strength));
+      AttackEvent((int)(amount - strength));
     }
 
     public void AlterHealth(int amount) {
--- Project/MyGameLibrary/Player.cs
+++ Project/MyGameLibrary/Player.cs
@@ -13,19 +13,20 @@ namespace Fall2020_CSC403_Project.code {
     public Player(Vector2 initPos, Collider collider) : base(initPos, collider) {
             this.experience = 0;
             this.level = 0;
-
             //helps calculate the player's level;
             //can be changed to increase how much experience is needed before leveling up
-            this.experiencePerLevel = 30;
+            this.experiencePerLevel = 20;
     }
     public void EarnExperience(int experienceToGain) {
             this.experience += experienceToGain;
             
             if (this.experience / this.experiencePerLevel >= 1 )
             {
-                //reset experience to 0 so it's easy to repeat the procedure above
                 this.experience = 0;
                 this.level += 1;
+                this.MaxHealth += 5;
+                this.Health += 5; //player gets a chance to heal a little after leveling up
+                this.strength += 2;
             }
     }
   }
