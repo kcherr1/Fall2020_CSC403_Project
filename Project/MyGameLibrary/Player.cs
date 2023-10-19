adfa644b6c88b1096e739e1f29b5f75bb1ba27ac
Jonah Fitzgerald <jonahfitzgerald0113@gmail.com>
2023-10-18 20:38:52 -0500

    Added a check in the FrmBattle file for giving experience to the Player when the battle ends and the enemy is defeated

--- Project/MyGameLibrary/Player.cs
+++ Project/MyGameLibrary/Player.cs
@@ -6,8 +6,27 @@ using System.Threading.Tasks;
 
 namespace Fall2020_CSC403_Project.code {
   public class Player : BattleCharacter {
-    public Player(Vector2 initPos, Collider collider) : base(initPos, collider) {
 
+    public int level { get; private set; }
+    public int experiencePerLevel { get; private set; }
+
+    public Player(Vector2 initPos, Collider collider) : base(initPos, collider) {
+            this.experience = 0;
+            this.level = 0;
+
+            //helps calculate the player's level;
+            //can be changed to increase how much experience is needed before leveling up
+            this.experiencePerLevel = 30;
+    }
+    public void EarnExperience(int experienceToGain) {
+            this.experience += experienceToGain;
+            
+            if (this.experience / this.experiencePerLevel >= 1 )
+            {
+                //reset experience to 0 so it's easy to repeat the procedure above
+                this.experience = 0;
+                this.level += 1;
+            }
     }
   }
 }
