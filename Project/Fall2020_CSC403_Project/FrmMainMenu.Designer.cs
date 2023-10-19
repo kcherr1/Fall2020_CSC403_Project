993080be824b6ef05503128982abd6271e7b1053
reberaidan <aidanweinreber@hotmail.com>
2023-10-18 21:27:32 -0500

    Main Menu Basic
    Added Main Menu Form. Game will launch with this form as the start.

---
+++ Project/Fall2020_CSC403_Project/FrmMainMenu.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace Fall2020_CSC403_Project
+{
+    partial class FrmMainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStartGame = new System.Windows.Forms.Button();
+            this.btnLeaveGame = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            //
+            // btnStartGame
+            //
+            this.btnStartGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartGame.Location = new System.Drawing.Point(250, 300);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(128, 43);
+            this.btnStartGame.TabIndex = 2;
+            this.btnStartGame.Text = "Start";
+            this.btnStartGame.UseVisualStyleBackColor = true;
+            this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
+            //
+            // btnLeaveGame
+            //
+            this.btnLeaveGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLeaveGame.Location = new System.Drawing.Point(250, 360);
+            this.btnLeaveGame.Name = "btnLeaveGame";
+            this.btnLeaveGame.Size = new System.Drawing.Size(128, 43);
+            this.btnLeaveGame.TabIndex = 2;
+            this.btnLeaveGame.Text = "Quit";
+            this.btnLeaveGame.UseVisualStyleBackColor = true;
+            this.btnLeaveGame.Click += new System.EventHandler(this.btnLeaveGame_Click);
+            // 
+            // lblEnemyHealthFull
+            // 
+            this.lblTitle.BackColor = System.Drawing.Color.Blue;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(200, 60);
+            this.lblTitle.Name = "lblEnemyHealthFull";
+            this.lblTitle.Size = new System.Drawing.Size(226, 20);
+            this.lblTitle.TabIndex = 6;
+            this.lblTitle.Text = "Title";
+
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnStartGame);
+            this.Controls.Add(this.btnLeaveGame);
+            this.Name = "FrmMainMenu";
+            this.Text = "Main Menu";
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox peanut;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnStartGame;
+        private System.Windows.Forms.Button btnLeaveGame;
+    }
+}
---
+++ Project/Fall2020_CSC403_Project/FrmMainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fall2020_CSC403_Project
+{
+    public partial class FrmMainMenu : Form
+    {
+        public static FrmMainMenu instance = null;
+
+        public FrmMainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            FrmLevel frmLevel = new FrmLevel();
+            frmLevel.Show();
+
+            //this.Close();
+        }
+
+        private void btnLeaveGame_Click(object sender, EventArgs e)
+        {
+            instance = null;
+            Close();
+        }
+    }
+}
