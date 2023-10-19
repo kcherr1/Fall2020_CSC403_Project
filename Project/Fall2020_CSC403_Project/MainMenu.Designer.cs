2b49ee2bea8fdee54e5c640aed6d1bc0ac999277
Samrinadhami00 <sdh065@email.latech.edu>
2023-10-18 23:59:23 -0500

    Added MainMenu class and UI.

---
+++ Project/Fall2020_CSC403_Project/MainMenu.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Fall2020_CSC403_Project
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.settingsButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Green;
+            this.startButton.Location = new System.Drawing.Point(324, 171);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(119, 71);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButtonClick);
+            // 
+            // quitButton
+            // 
+            this.quitButton.BackColor = System.Drawing.Color.Red;
+            this.quitButton.Location = new System.Drawing.Point(324, 415);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(119, 65);
+            this.quitButton.TabIndex = 1;
+            this.quitButton.Text = "QUIT";
+            this.quitButton.UseVisualStyleBackColor = true;
+            this.quitButton.Click += new System.EventHandler(this.quitButtonClick);
+            // 
+            // settingsButton
+            // 
+            this.settingsButton.Location = new System.Drawing.Point(324, 291);
+            this.settingsButton.Name = "settingsButton";
+            this.settingsButton.Size = new System.Drawing.Size(119, 64);
+            this.settingsButton.TabIndex = 2;
+            this.settingsButton.Text = "SETTINGS";
+            this.settingsButton.UseVisualStyleBackColor = true;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 794);
+            this.Controls.Add(this.settingsButton);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.startButton);
+            this.Name = "MainMenu";
+            this.Text = "MainMenu";
+            this.Load += new System.EventHandler(this.MainMenu_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.Button settingsButton;
+    }
+}
---
+++ Project/Fall2020_CSC403_Project/MainMenu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fall2020_CSC403_Project
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+        
+        /// <summary>
+        /// Method to start the game 
+        /// </summary>
+        private void startButtonClick(object sender, EventArgs e)
+        {   
+            // Create FrmLevel and show it
+            FrmLevel frm = new FrmLevel();
+            frm.Show();
+            // Whenever FrmLevel is closed, execute onFormClosed method
+            frm.FormClosed += frm.onFormClosed;
+            Hide();
+
+        }
+
+        /// <summary>
+        /// Method to handle closing game from main menu
+        /// </summary>
+        private void quitButtonClick(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void MainMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
