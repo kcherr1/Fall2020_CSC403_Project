332812e68e70c22072d019a5e1b7f36cc9a12384
CaryRandazzo <randazzocary@gmail.com>
2023-10-19 23:32:36 -0500

    Added textbox, scrolling text on startup

--- Project/Fall2020_CSC403_Project/Program.cs
+++ Project/Fall2020_CSC403_Project/Program.cs
@@ -5,15 +5,19 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Fall2020_CSC403_Project {
-  static class Program {
-    /// <summary>
-    /// The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main() {
-      Application.EnableVisualStyles();
-      Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new FrmLevel());
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+
+        [STAThread]
+        static void Main() 
+        {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+                //Application.Run(new FrmLevel());
+        }
     }
-  }
 }
