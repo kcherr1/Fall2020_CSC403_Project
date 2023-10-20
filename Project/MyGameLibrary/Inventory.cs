28dd09ec6c250d2a6d0fba0d37314c748ba2ab3f
LaTechBen <bjm056@email.latech.edu>
2023-10-20 14:20:28 -0500

    Created an Inventory class, Item class and HealthItem class.

---
+++ Project/MyGameLibrary/Inventory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2020_CSC403_Project.code
+{
+    public class Inventory
+    {
+        private Dictionary<string, Item> itemstorage = new Dictionary<string, Item>();
+        public bool visible = false;
+
+        public Inventory()
+        { }
+
+        public void addItem(Item item)
+        {
+            itemstorage.Add(item.NAME, item);
+        }
+
+        public void removeItem(Item item)
+        {
+            if (itemstorage.ContainsKey(item.NAME))
+            {
+                itemstorage.Remove(item.NAME);
+            }
+        }
+    }
+}
---
+++ Project/MyGameLibrary/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2020_CSC403_Project.code
+{
+    public class Item : Character
+    {
+        public string NAME;
+        public Item(Vector2 initPos, Collider collider, string name) : base(initPos, collider)
+        {
+            NAME = name;
+        }
+
+        public void useItem()
+        { }
+        
+    }
+}
