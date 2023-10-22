d57bf4c4bc12b2d06274f84d29249130e122dc4c
BlueJayBird11 <51869746+BlueJayBird11@users.noreply.github.com>
2023-10-22 16:12:48 -0500

    Added Dialogue class

---
+++ Project/MyGameLibrary/Dialogue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2020_CSC403_Project.code
+{
+    public class Dialogue
+    {
+        private string[] lines;
+        private int[] letterSpeeds;
+        private int lineNumber = 0;
+        // DialogueBox nextDialogueBox = null;
+        public Dialogue(string[] lines, int[] letterSpeeds) {
+            this.lines = lines;
+            this.letterSpeeds = letterSpeeds;
+            
+        }
+
+        public bool IsLastLine()
+        {
+            if (lines.Length == lineNumber)
+            {
+                lineNumber = 0;
+                return true;
+            }
+            return false;
+        }
+
+        public String GetNextLine () {
+            Console.WriteLine(lineNumber);
+            String lineToReturn = lines[lineNumber];
+            lineNumber++;
+            return lineToReturn;
+        }
+
+        public int GetLineSpeed () { 
+            return letterSpeeds[lineNumber - 1];
+        }
+        
+    }
+}
