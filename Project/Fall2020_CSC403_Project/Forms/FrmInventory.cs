e6bd4081920fd3897a541c883d8ba446f5a5e359
FoldsAbigail <afo022@latech.edu>
2023-10-24 21:26:01 -0500

    Added the menu form
    Created the menu form

---
+++ Project/Fall2020_CSC403_Project/Forms/FrmInventory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Fall2020_CSC403_Project.code;
+
+namespace Fall2020_CSC403_Project
+{
+    public partial class FrmInventory : Form
+    {
+        public static FrmInventory instance = null;
+        public static FrmInventory GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new FrmInventory();
+            }
+            return instance;
+        }
+
+        public FrmInventory()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmInventory_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.I:
+                    Hide();
+                    break;
+                case Keys.Escape:
+                    Hide();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Project/Fall2020_CSC403_Project/Forms/FrmInventory.designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Fall2020_CSC403_Project
+{
+    partial class FrmInventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.invSlot1 = new System.Windows.Forms.PictureBox();
+            this.invSlot2 = new System.Windows.Forms.PictureBox();
+            this.invSlot6 = new System.Windows.Forms.PictureBox();
+            this.invSlot5 = new System.Windows.Forms.PictureBox();
+            this.invSlot10 = new System.Windows.Forms.PictureBox();
+            this.invSlot9 = new System.Windows.Forms.PictureBox();
+            this.invSlot12 = new System.Windows.Forms.PictureBox();
+            this.invSlot11 = new System.Windows.Forms.PictureBox();
+            this.invSlot8 = new System.Windows.Forms.PictureBox();
+            this.invSlot7 = new System.Windows.Forms.PictureBox();
+            this.invSlot4 = new System.Windows.Forms.PictureBox();
+            this.invSlot3 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // invSlot1
+            // 
+            this.invSlot1.BackColor = System.Drawing.Color.Transparent;
+            this.invSlot1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.invSlot1.Location = new System.Drawing.Point(55, 143);
+            this.invSlot1.Name = "invSlot1";
+            this.invSlot1.Size = new System.Drawing.Size(111, 111);
+            this.invSlot1.TabIndex = 0;
+            this.invSlot1.TabStop = false;
+            // 
+            // invSlot2
+            // 
+            this.invSlot2.BackColor = System.Drawing.Color.Transparent;
+            this.invSlot2.Location = new System.Drawing.Point(204, 143);
+            this.invSlot2.Name = "invSlot2";
+            this.invSlot2.Size = new System.Drawing.Size(111, 111);
+            this.invSlot2.TabIndex = 1;
+            this.invSlot2.TabStop = false;
+            // 
+            // invSlot6
+            // 
+            this.invSlot6.BackColor = System.Drawing.Color.Transparent;
+            this.invSlot6.Location = new System.Drawing.Point(204, 288);
+            this.invSlot6.Name = "invSlot6";
+            this.invSlot6.Size = new System.Drawing.Size(111, 111);
+            this.invSlot6.TabIndex = 3;
+            this.invSlot6.TabStop = false;
+            // 
+            // invSlot5
+            // 
+            this.invSlot5.BackColor = System.Drawing.Color.Transparent;
+            this.invSlot5.Location = new System.Drawing.Point(55, 288);
+            this.invSlot5.Name = "invSlot5";
+            this.invSlot5.Size = new System.Drawing.Size(111, 111);
+            this.invSlot5.TabIndex = 2;
+            this.invSlot5.TabStop = false;
+            // 
+            // invSlot10
+            // 
+            this.invSlot10.BackColor = System.Drawing.Color.Transparent;
+            this.invSlot10.Location = new System.Drawing.Point(204, 430);
+            this.invSlot10.Name = "invSlot10";
+            this.invSlot10.Size = new System.Drawing.Size(111, 111);
+            this.invSlot10.TabIndex = 5;
+            this.invSlot10.TabStop = false;
+            // 
+            // invSlot9
+            // 
+            this.invSlot9.BackColor = System.Drawing.Color.Transparent;
+            this.invSlot9.Location = new System.Drawing.Point(55, 430);
+            this.invSlot9.Name = "invSlot9";
+            this.invSlot9.Size = new System.Drawing.Size(111, 111);
+            this.invSlot9.TabIndex = 4;
+            this.invSlot9.TabStop = false;
+            // 
+            // invSlot12
+            // 
+            this.invSlot12.BackColor = System.Drawing.Color.Transparent;
+            this.invSlot12.Location = new System.Drawing.Point(505, 430);
+            this.invSlot12.Name = "invSlot12";
+            this.invSlot12.Size = new System.Drawing.Size(111, 111);
+            this.invSlot12.TabIndex = 11;
+            this.invSlot12.TabStop = false;
+            // 
+            // invSlot11
+            // 
+            this.invSlot11.BackColor = System.Drawing.Color.Transparent;
+            this.invSlot11.Location = new System.Drawing.Point(356, 430);
+            this.invSlot11.Name = "invSlot11";
+            this.invSlot11.Size = new System.Drawing.Size(111, 111);
+            this.invSlot11.TabIndex = 10;
+            this.invSlot11.TabStop = false;
+            // 
+            // invSlot8
+            // 
+            this.invSlot8.BackColor = System.Drawing.Color.Transparent;
+            this.invSlot8.Location = new System.Drawing.Point(505, 288);
+            this.invSlot8.Name = "invSlot8";
+            this.invSlot8.Size = new System.Drawing.Size(111, 111);
+            this.invSlot8.TabIndex = 9;
+            this.invSlot8.TabStop = false;
+            // 
+            // invSlot7
+            // 
+            this.invSlot7.BackColor = System.Drawing.Color.Transparent;
+            this.invSlot7.Location = new System.Drawing.Point(356, 288);
+            this.invSlot7.Name = "invSlot7";
+            this.invSlot7.Size = new System.Drawing.Size(111, 111);
+            this.invSlot7.TabIndex = 8;
+            this.invSlot7.TabStop = false;
+            // 
+            // invSlot4
+            // 
+            this.invSlot4.BackColor = System.Drawing.Color.Transparent;
+            this.invSlot4.Location = new System.Drawing.Point(505, 143);
+            this.invSlot4.Name = "invSlot4";
+            this.invSlot4.Size = new System.Drawing.Size(111, 111);
+            this.invSlot4.TabIndex = 7;
+            this.invSlot4.TabStop = false;
+            // 
+            // invSlot3
+            // 
+            this.invSlot3.BackColor = System.Drawing.Color.Transparent;
+            this.invSlot3.Location = new System.Drawing.Point(356, 143);
+            this.invSlot3.Name = "invSlot3";
+            this.invSlot3.Size = new System.Drawing.Size(111, 111);
+            this.invSlot3.TabIndex = 6;
+            this.invSlot3.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(657, 94);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(419, 483);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // FrmInventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OliveDrab;
+            this.ClientSize = new System.Drawing.Size(1111, 613);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.invSlot12);
+            this.Controls.Add(this.invSlot11);
+            this.Controls.Add(this.invSlot8);
+            this.Controls.Add(this.invSlot7);
+            this.Controls.Add(this.invSlot4);
+            this.Controls.Add(this.invSlot3);
+            this.Controls.Add(this.invSlot10);
+            this.Controls.Add(this.invSlot9);
+            this.Controls.Add(this.invSlot6);
+            this.Controls.Add(this.invSlot5);
+            this.Controls.Add(this.invSlot2);
+            this.Controls.Add(this.invSlot1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "FrmInventory";
+            this.Text = "FrmInventory";
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.invSlot3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox invSlot1;
+        private System.Windows.Forms.PictureBox invSlot2;
+        private System.Windows.Forms.PictureBox invSlot6;
+        private System.Windows.Forms.PictureBox invSlot5;
+        private System.Windows.Forms.PictureBox invSlot10;
+        private System.Windows.Forms.PictureBox invSlot9;
+        private System.Windows.Forms.PictureBox invSlot12;
+        private System.Windows.Forms.PictureBox invSlot11;
+        private System.Windows.Forms.PictureBox invSlot8;
+        private System.Windows.Forms.PictureBox invSlot7;
+        private System.Windows.Forms.PictureBox invSlot4;
+        private System.Windows.Forms.PictureBox invSlot3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
