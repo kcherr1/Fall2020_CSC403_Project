ba655f83e303bd4ba487f74b530b8f1024745125
briannaosms <brianna.osms@protonmail.com>
2023-10-26 13:12:32 -0500

    added level textures, changed level 1 bgnd image, made new level and game state class

---
+++ Project/MyGameLibrary/GameState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Fall2020_CSC403_Project.code {
+  public class GameState {
+    public Player player { get; private set; }
+    public DateTime timeStart { get; private set; }
+    //public Enemy[] enemies { get; set; }
+
+    public GameState(Player player, DateTime timeStart) {
+      this.player = player;
+      this.timeStart = timeStart;
+      //this.enemies = enemies;
+    }
+  }
+}
