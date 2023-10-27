828cafaa897ef95576efb57df509c35f2fe4a7ee
morgan.nikolas <nmorgan5055@gmail.com>
2023-10-26 20:19:56 -0500

    Fixed image bug again

--- Project/Fall2020_CSC403_Project/FrmInv.cs
+++ Project/Fall2020_CSC403_Project/FrmInv.cs
@@ -57,11 +57,11 @@ namespace Fall2020_CSC403_Project
             }
             if (player.Inventory.Armor != null)
             {
-                Armor.Image = player.Inventory.Weapon.Pic.Image;
+                Armor.Image = player.Inventory.Armor.Pic.Image;
             }
             if (player.Inventory.Utility != null)
             {
-                Utility.Image = player.Inventory.Weapon.Pic.Image;
+                Utility.Image = player.Inventory.Utility.Pic.Image;
             }
 
 
