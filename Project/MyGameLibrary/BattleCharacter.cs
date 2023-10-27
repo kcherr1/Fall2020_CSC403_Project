8cdf1d3b8a571e1727cea36fbc978d3b3c2877ae
Beanless <jwo011@email.latech.edu>
2023-10-27 14:47:19 -0500

    fixed a merge issue

--- Project/MyGameLibrary/BattleCharacter.cs
+++ Project/MyGameLibrary/BattleCharacter.cs
@@ -27,11 +27,12 @@ namespace Fall2020_CSC403_Project.code {
         public void AlterHealth(int amount) {
             Health += amount;
         }
-        
+
         // gets strength value from player for display in CharacterScreen.cs
         public float getStrength()
         {
             return strength;
+        }
           
         //added a set health function that sets the health of the character chosen to a specific integer
         public void SetHealth(int amount)
