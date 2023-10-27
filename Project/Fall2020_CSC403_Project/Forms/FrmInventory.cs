21438cdb6ae351028c30b26dd46cd07ffc20c9a2
FoldsAbigail <afo022@latech.edu>
2023-10-26 23:15:01 -0500

    Added ambiguity to adding Images
    Added it so that it will cycle through the items and put them in the slots.
    Also fixed a bug that crashed the game if the inventory was empty.

--- Project/Fall2020_CSC403_Project/Forms/FrmInventory.cs
+++ Project/Fall2020_CSC403_Project/Forms/FrmInventory.cs
@@ -14,14 +14,34 @@ namespace Fall2020_CSC403_Project
     public partial class FrmInventory : Form
     {
         public static FrmInventory instance = null;
+        public static List<PictureBox> pictureBoxSlots = new List<PictureBox>();
         public static FrmInventory GetInstance(Inventory inventory)
         {
             if (instance == null)
             {
                 instance = new FrmInventory();
-                instance.invSlot1.BackgroundImage = inventory.GetItems()[0].Img;
+                DisplayInventory(inventory);
+                
             }
             return instance;
+        }
+
+        public static void DisplayInventory(Inventory inventory)
+        {
+            List<Item> items = inventory.GetItems(); // Replace this with your actual method to get items
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                PictureBox slot = pictureBoxSlots[i];
+                Item item = items[i];
+
+                if (slot != null && item != null)
+                {
+                    // Set the item's name as text and its image as the background for the slot
+                    slot.Text = item.Name;
+                    slot.BackgroundImage = item.Img;
+                }
+            }
         }
 
         public FrmInventory()
@@ -29,6 +49,16 @@ namespace Fall2020_CSC403_Project
             InitializeComponent();
             this.KeyDown += FrmInventory_KeyDown;
             this.KeyPreview = true;
+
+            // Add all PictureBox controls on the form to the pictureBoxSlots list
+            for (int i = 1; i<=12; i++)
+            {
+                PictureBox pic = Controls.Find("invSlot" + i, true).FirstOrDefault() as PictureBox;
+                if (pic != null)
+                {
+                    pictureBoxSlots.Add(pic);
+                }
+            }
         }
 
         private void FrmInventory_KeyDown(object sender, KeyEventArgs e)
