76322c6bf4eea0b1de3f9a5208ecdf2bb4b3f8f1
Garrett <garrettlee0716@gmail.com>
2023-10-30 13:56:42 -0500

    Fixed bug which would crash on startup

--- Project/Fall2020_CSC403_Project/Properties/Resources.Designer.cs
+++ Project/Fall2020_CSC403_Project/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Fall2020_CSC403_Project.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap deadenemy_poisonpacket_fw {
+            get {
+                object obj = ResourceManager.GetObject("deadenemy_poisonpacket_fw", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap enemy_cheetos {
             get {
                 object obj = ResourceManager.GetObject("enemy_cheetos", resourceCulture);
