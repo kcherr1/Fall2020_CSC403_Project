97f0b5da9714d06379573197ae2e0df8852c5275
Jacob <johnboyjacob1@gmail.com>
2023-11-04 21:57:25 -0500

    adding a confirm quit popup
    so players can not accidentally click quit,  I added a confirm quit form that pops up when user clicks the quit button in settings

---
+++ Project/Fall2020_CSC403_Project/Forms/ConfirmQuit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fall2020_CSC403_Project.Forms
+{
+    public partial class ConfirmQuit : Form
+    {
+        public ConfirmQuit()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
