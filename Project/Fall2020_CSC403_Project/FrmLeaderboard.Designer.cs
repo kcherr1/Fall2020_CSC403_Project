303bfc60ca257af3df5fa900fb65409be9368650
Joseph990 <jtb072@email.latech.edu>
2023-11-05 18:58:25 -0600

    Fixed leaderboard sizing issue?

--- Project/Fall2020_CSC403_Project/FrmLeaderboard.Designer.cs
+++ Project/Fall2020_CSC403_Project/FrmLeaderboard.Designer.cs
@@ -36,7 +36,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmLeaderboard";
             this.Text = "FrmLeaderboard";
             this.ResumeLayout(false);
