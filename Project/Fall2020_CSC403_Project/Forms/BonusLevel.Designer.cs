a573b54e9d1839ebf0e47124429b3a8c7db3e977
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2023-11-08 14:39:06 -0600

    added background to bonus level

--- Project/Fall2020_CSC403_Project/Forms/BonusLevel.Designer.cs
+++ Project/Fall2020_CSC403_Project/Forms/BonusLevel.Designer.cs
@@ -28,19 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.floor1 = new System.Windows.Forms.PictureBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.floor1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // floor1
+            // 
+            this.floor1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.floor1.Location = new System.Drawing.Point(-2, 832);
+            this.floor1.Name = "floor1";
+            this.floor1.Size = new System.Drawing.Size(1575, 63);
+            this.floor1.TabIndex = 0;
+            this.floor1.TabStop = false;
             // 
             // BonusLevel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.ClientSize = new System.Drawing.Size(1568, 894);
+            this.Controls.Add(this.floor1);
             this.Name = "BonusLevel";
             this.Text = "BonusLevel";
+            ((System.ComponentModel.ISupportInitialize)(this.floor1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox floor1;
+        private System.Windows.Forms.FontDialog fontDialog1;
     }
 }
