647162a117ebf989b60209103e57c8574ffa0834
SethajWarren <sethajwarren@gmail.com>
2023-11-09 15:41:48 -0600

    Added NPCs to the scren

--- Project/Fall2020_CSC403_Project/FrmLevel.cs
+++ Project/Fall2020_CSC403_Project/FrmLevel.cs
@@ -1031,6 +1031,8 @@ namespace Fall2020_CSC403_Project
             Game.CurrentArea.AddEnemy(Game.Enemies["Brute"]);
 
             Game.CurrentArea.AddNPC(Game.NPCs["Harold"]);
+            Game.CurrentArea.AddNPC(Game.NPCs["Gerald"]);
+            Game.CurrentArea.AddNPC(Game.NPCs["Tombstone"]);
         }
 
         private void Area3()
