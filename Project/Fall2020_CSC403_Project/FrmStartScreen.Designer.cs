71c3252c912be60ea01175871117ab805c115404
Jonah Fitzgerald <jonahfitzgerald0113@gmail.com>
2023-11-09 15:10:08 -0600

    fixed it so the arrow keys shift focus on the buttons on the start screen in an intuitive order

--- Project/Fall2020_CSC403_Project/FrmStartScreen.Designer.cs
+++ Project/Fall2020_CSC403_Project/FrmStartScreen.Designer.cs
@@ -58,6 +58,7 @@
       this.button1.Name = "button1";
       this.button1.Size = new System.Drawing.Size(98, 41);
       this.button1.TabIndex = 1;
+      this.button1.TabStop = false;
       this.button1.Text = "Start";
       this.button1.UseVisualStyleBackColor = false;
       this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -68,7 +69,8 @@
       this.button2.Location = new System.Drawing.Point(716, 556);
       this.button2.Name = "button2";
       this.button2.Size = new System.Drawing.Size(98, 41);
-      this.button2.TabIndex = 2;
+      this.button2.TabIndex = 3;
+      this.button2.TabStop = false;
       this.button2.Text = "Exit";
       this.button2.UseVisualStyleBackColor = false;
       this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -90,7 +92,8 @@
       this.button3.Location = new System.Drawing.Point(591, 556);
       this.button3.Name = "button3";
       this.button3.Size = new System.Drawing.Size(98, 41);
-      this.button3.TabIndex = 5;
+      this.button3.TabIndex = 2;
+      this.button3.TabStop = false;
       this.button3.Text = "Controls";
       this.button3.UseVisualStyleBackColor = false;
       this.button3.Click += new System.EventHandler(this.button3_Click);
