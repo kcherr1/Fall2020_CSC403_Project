44fb18cdd64a522e6d2b689e11812611407a5b7c
Suraj-1729 <suraj.191810@gmail.com>
2023-11-15 03:23:31 +0530

    added FrmFAQ.cs

---
+++ Project/Fall2020_CSC403_Project/FAQ.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace Fall2020_CSC403_Project
+{
+    partial class FrmFAQ
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FAQ
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.BackgroundForRPG3;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.Name = "FAQ";
+            this.Text = "FAQ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Project/Fall2020_CSC403_Project/FAQ.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fall2020_CSC403_Project
+{
+    public partial class FrmFAQ : Form
+    {
+        public FrmFAQ(Form previous)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            InitializeComponent();
+            this.setup();
+        }
+        private void setup()
+        {
+            int height = Screen.PrimaryScreen.Bounds.Height;
+            int width = Screen.PrimaryScreen.Bounds.Width;
+
+            //Adding back button and main menu button
+            Button backButton = new Button();
+            Button homeButton = new Button();
+
+            backButton.Location = new Point(width / 18, height / 12);
+            backButton.AutoSize = true;
+            //backButton.Image = Properties.Resources.
+            homeButton.Location = new Point(16 * width / 18, height / 12);
+            homeButton.AutoSize = true;
+            
+            // Heading Label for FAQ's
+            Label headingLabel = new Label();
+            headingLabel.Text = "Frequently Asked Questions";
+            headingLabel.Font = new Font("Century Gothic", 24, FontStyle.Bold);
+            headingLabel.BackColor = Color.Transparent;
+            headingLabel.Location = new Point((this.ClientSize.Width - headingLabel.Width) / 2, height / 10);
+            headingLabel.AutoSize = true;
+
+            this.Controls.Add(headingLabel);
+        }
+
+    }
+}
